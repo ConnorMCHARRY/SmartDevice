83197c0620a2e8f3e9a248c99c0cb2267a389534
ConnorMCHARRY <4200184@schoolsnet.act.edu.au>
2023-05-15 20:16:35 +1000

    arduino code
    beginning

--- SmartDevice/SmartDevice.ino
+++ SmartDevice/SmartDevice.ino
@@ -8,7 +8,7 @@ RTC_Millis rtc;     // Software Real Time Clock (RTC)
 DateTime rightNow;  // used to store the current time.
 
 // SD Card - Confirm Pin
-#define SDpin 53
+#define SDpin 10
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,4 +28,43 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  trafficLights();
+
+  delay(250);
+}
+
+/*
+ Using a timer to control the traffic lights.
+ @perams none
+ @return none
+ */
+void trafficLights() {
+
+}
+
+/*
+ Throttle engages when trafficLights turn green.
+ @perams trafficLights
+ @return throttle
+ */
+void throttle() {
+
+}
+
+/*
+ Using a sensor on the road to trigger the brake.
+ @perams sensor
+ @return none
+ */
+void brake() {
+
+}
+
+/*
+ sensor on the road waits for a vehicle to trigger itself.
+ @perams vehicle
+ @return brake
+ */
+void sensor() {
+
 }
--- design.md
+++ design.md
@@ -2,11 +2,14 @@
 
 ## Smart Device Theme
 
-> Include your theme here. Delete this comment.
+> My theme is a car, it will have traffic lights, a brake and a throttle, it won't have a turning system initially. Along the road will be a sensor to make the car hit the brakes when triggered.
 
 
 ## Behaviours
 
-> Add Behaviours here. Delete this comment.
 
-1. Behaviour 1
+1. Traffic light starts off red.
+2. Traffic light will eventually turn green.
+3. When the traffic light turns green, the car puts the throttle on.
+4. Car hits a sensor on the road that trigger the brakes.
+5. Car stops, End.
